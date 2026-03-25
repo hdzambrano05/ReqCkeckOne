8410ddbff7c7c7738b38c1adbda46cdfe38655d1
Harold Zambrano <hdzambrano.2350@unicesmag.edu.co>
2026-03-24 19:35:49 -0500

    ReqCkeckOne_v0.1.7

--- reqcheckone-cl/src/app/services/requirements.ts
+++ reqcheckone-cl/src/app/services/requirements.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class RequirementsService {
+  
   private apiUrl = `${environment.apiUrl}/requirements`;
   
 
